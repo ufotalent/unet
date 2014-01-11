acee708a70331fafbe53f403bd00f209b58fd857
Ufotalent <ufotalent.zju@gmail.com>
2014-01-11 22:28:34 +0800

    fix ping problem on mac os

--- ping.sh
+++ ping.sh
@@ -2,12 +2,12 @@
 while true; do
     while read line; do 
         ip=$(echo $line | awk '{print $2}')
-        ping -qc 60 $ip | tail -n2 > pings/$ip.new &
+        ping -qc 10 $ip | tail -n2 > pings/$ip.new &
     done < ip-name
     wait
     while read line; do 
         ip=$(echo $line | awk '{print $2}')
-        cat pings/$ip.new | grep loss | awk -F ',' '{for (i=1;i<NF;i++) print $i}'  | grep loss > pings/$ip
-        cat pings/$ip.new | grep rtt | awk -F '/' '{print $5}' >> pings/$ip 
+        cat pings/$ip.new | grep loss | awk -F ',' '{for (i=1;i<=NF;i++) print $i}'  | grep loss > pings/$ip
+        cat pings/$ip.new | grep avg | awk -F '/' '{print $5}' >> pings/$ip 
     done < ip-name
 done
